b2f4440ea923a052e9f0ed1abb13db95c2b6c134
Grad Vasile <vasile.grad2@gmail.com>
2018-02-25 01:00:04 +0200

    Product Index View

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,13 +3,16 @@
 <head>
 	<title>@yield('title')</title>
 	<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+	<link href="{{ URL::to('css/app.css') }}" rel="stylesheet">
 	@yield('styles')
 </head>
 <body>
-@yield('content')
+@include('partials.header')
+<div class="container">	
+	@yield('content')
+</div>
 
 <!-- Scripts -->
-
 <script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
 <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 @yield('scripts')
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -4,5 +4,86 @@
 	Laravel Shopping Cart
 @endsection
 @section('content')
-	<h1>It works!</h1>
+	<div class="row">
+	  <div class="col-sm-6 col-md-4">
+	    <div class="thumbnail">
+	      <img src="http://i0.wp.com/thehogshead.org/wp-content/uploads/2013/10/w620-1a3546192fa56326fabe069f8097c4c91.jpg?fit=604%2C893" alt="..." class="img-responsive">
+	      <div class="caption">
+	        <h3>Product Title</h3>
+	        <p class="description"> is taken from text that reads, "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit ...," which translates as, "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain.</p>
+	        <div class="clearfix">
+	        	<div class="pull-left price">$12</div>
+	        	<a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+	  <div class="col-sm-6 col-md-4">
+	    <div class="thumbnail">
+	      <img src="http://i0.wp.com/thehogshead.org/wp-content/uploads/2013/10/w620-1a3546192fa56326fabe069f8097c4c91.jpg?fit=604%2C893" alt="..." class="img-responsive">
+	      <div class="caption">
+	        <h3>Product Title</h3>
+	        <p class="description"> is taken from text that reads, "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit ...," which translates as, "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain.</p>
+	        <div class="clearfix">
+	        	<div class="pull-left price">$12</div>
+	        	<a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+	  <div class="col-sm-6 col-md-4">
+	    <div class="thumbnail">
+	      <img src="http://i0.wp.com/thehogshead.org/wp-content/uploads/2013/10/w620-1a3546192fa56326fabe069f8097c4c91.jpg?fit=604%2C893" alt="..." class="img-responsive">
+	      <div class="caption">
+	        <h3>Product Title</h3>
+	        <p class="description"> is taken from text that reads, "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit ...," which translates as, "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain.</p>
+	        <div class="clearfix">
+	        	<div class="pull-left price">$12</div>
+	        	<a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+	<div class="row">
+	  <div class="col-sm-6 col-md-4">
+	    <div class="thumbnail">
+	      <img src="http://i0.wp.com/thehogshead.org/wp-content/uploads/2013/10/w620-1a3546192fa56326fabe069f8097c4c91.jpg?fit=604%2C893" alt="..." class="img-responsive">
+	      <div class="caption">
+	        <h3>Product Title</h3>
+	        <p class="description"> is taken from text that reads, "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit ...," which translates as, "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain.</p>
+	        <div class="clearfix">
+	        	<div class="pull-left price">$12</div>
+	        	<a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+	  <div class="col-sm-6 col-md-4">
+	    <div class="thumbnail">
+	      <img src="http://i0.wp.com/thehogshead.org/wp-content/uploads/2013/10/w620-1a3546192fa56326fabe069f8097c4c91.jpg?fit=604%2C893" alt="..." class="img-responsive">
+	      <div class="caption">
+	        <h3>Product Title</h3>
+	        <p class="description"> is taken from text that reads, "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit ...," which translates as, "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain.</p>
+	        <div class="clearfix">
+	        	<div class="pull-left price">$12</div>
+	        	<a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+	  <div class="col-sm-6 col-md-4">
+	    <div class="thumbnail">
+	      <img src="http://i0.wp.com/thehogshead.org/wp-content/uploads/2013/10/w620-1a3546192fa56326fabe069f8097c4c91.jpg?fit=604%2C893" alt="..." class="img-responsive">
+	      <div class="caption">
+	        <h3>Product Title</h3>
+	        <p class="description"> is taken from text that reads, "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit ...," which translates as, "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain.</p>
+	        <div class="clearfix">
+	        	<div class="pull-left price">$12</div>
+	        	<a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+	</div>
 @endsection
